257467048c7a519dbf68cf673fad866379a0e050
Subhan Ali <ali.023@gmail.com>
2017-02-12 15:18:40 -0600

    changes permissions for heroku

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var account = db.account;
 
 // Sets up the Express App
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
